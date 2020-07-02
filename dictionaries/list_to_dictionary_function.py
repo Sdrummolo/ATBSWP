c290423c8dd7fa4a479c2fc6e20fec2871704e24
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-07-02 20:22:59 +0100

    added dictionaries projects

--- dictionaries/list_to_dictionary_function.py
+++
@@ -1,7 +0,0 @@
-def addToInventory(inventory, addedItems):
-
-
-inv = {'gold coin': 42, 'rope': 1}
-dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-inv = addToInventory(inv, dragonLoot)
-displayInventory(inv)
